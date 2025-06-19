61654d2af648a308e1a17d53f9c88de7458048e5
Ivan Melnyk <ivmelnyk@griddynamics.com>
2025-06-19 14:00:32 +0200

    feat: setup basic http server

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/t3nna/rssagg
 
 go 1.24.3
+
+require github.com/joho/godotenv v1.5.1
+
+require github.com/go-chi/chi v1.5.5
+
+require github.com/go-chi/cors v1.2.1
--- main.go
+++ main.go
@@ -1,8 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
 
-func main(){
-fmt.Println("Hello World")
+func main() {
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("No .env file was founded")
+	}
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+	log.Printf("Server starting on port %s", portString)
+	err = srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
